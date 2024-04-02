84dff23d3f75e728749d7100d0bfd590c3bd3384
Barış <irfanbarisozer@gmail.com>
2024-04-02 12:43:48 +0300

    create function implemented

--- ProposalContract.sol
+++ ProposalContract.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.18;
 contract ProposalContract {
+    uint256 private counter;
     struct Proposal {
         string title; // Title of the proposal
         string description; // Description of the proposal
@@ -15,4 +16,9 @@ contract ProposalContract {
     }
 
     mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
+
+    function create(string calldata _title, string calldata _description, uint256 _total_vote_to_end) external {
+        counter += 1;
+        proposal_history[counter] = Proposal(_title,_description, 0,0,0, _total_vote_to_end, false, true);
+    }
 }
